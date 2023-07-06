6896b13778d1cf2f222424c0f5d4c1c7efd87b82
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-06 21:20:35 +0100

    Refactored version of the completed JobExpenses task

--- JobExpenses/Program.cs
+++ JobExpenses/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Expenses
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int ArrayLength;
+
+            ArrayLength = int.Parse(Console.ReadLine());
+
+            if (ArrayLength == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            string TransactionsString = Console.ReadLine();
+            string[] Transactions = TransactionsString.Split();
+
+            int SumOfExpenses = 0;
+
+            for (int i = 0; i < Transactions.Length; i++)
+            {
+                if ((int.Parse(Transactions[i])) < 0)
+                {
+                    SumOfExpenses += int.Parse(Transactions[i]);
+                }
+            }
+
+            if (SumOfExpenses != 0)
+            {
+                int SumOfExpensesAsPositiveValue = SumOfExpenses * -1;
+
+                Console.WriteLine(SumOfExpensesAsPositiveValue);
+            }
+
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
 
