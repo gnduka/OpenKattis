6b9094c972166bf13453a37199edb9c3e3b0067f
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-06 22:46:41 +0100

    Successful completion of the SortTwoNumbers task.

---
+++ SortTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SortTwoNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputString = Console.ReadLine();
+            string[] inputs = inputString.Split(' ');
+
+            if (int.Parse(inputs[0]) < int.Parse(inputs[1]))
+            {
+                Console.WriteLine(inputs[0] + " " + inputs[1]);
+            }
+
+            else
+            {
+                Console.WriteLine(inputs[1] + " " + inputs[0]);
+            }
+        }
+    }
+}
+
