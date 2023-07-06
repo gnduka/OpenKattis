71e8ef5208b16666fe78856a8bf44752745c6ef3
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-06 22:23:22 +0100

    Successful completion of the Aaah task.

---
+++ Aaaah/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aaaah
+{
+    class Program
+    {
+        static void Main()
+        {
+            string FirstInput = Console.ReadLine();
+            string SecondInput = Console.ReadLine();
+
+            if (FirstInput.Length >= SecondInput.Length)
+            {
+                Console.WriteLine("go");
+            }
+
+            else if (SecondInput.Length > FirstInput.Length)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
