af473de277a94bf9d54b13ef7cb14c86f59634a3
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-06 22:40:10 +0100

    Successful completion of the FindingAnA task.

---
+++ FindingAnA/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        public static string inputString;
+
+        static void Main()
+        {
+            inputString = Console.ReadLine();
+            int counter = 0;
+
+            foreach (char c in inputString)
+            {
+                if (c != 'a')
+                {
+                    counter++;
+                    continue;
+                }
+
+                else
+                {
+                    Display(counter);
+                    return;
+                }
+            }
+        }
+
+        static void Display(int startingPoint)
+        {
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (startingPoint < inputString.Length)
+                {
+                    Console.Write(inputString[startingPoint]);
+                    startingPoint++;
+                }
+
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
+
