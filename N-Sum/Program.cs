f14eacfa4db039bc7ba433a820b12294ffda32d6
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-10 02:45:30 +0100

    N-Sum complete

---
+++ N-Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int NumberofIntegers = int.Parse(Console.ReadLine());
+            string inputString = Console.ReadLine();
+
+            string[] inputs = inputString.Split();
+            int output = 0;
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                output += int.Parse(inputs[i]);
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
+
