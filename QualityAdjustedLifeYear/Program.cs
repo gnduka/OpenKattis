eb82c493491e9e325c1157c33c1a9ab2ece13a95
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-10 02:24:10 +0100

    Quality-AdjustedLifeYear complete

---
+++ QualityAdjustedLifeYear/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LifeYear
+{
+    class Program
+    {
+        static void Main()
+        {
+            int NumberOfInputs = int.Parse(Console.ReadLine());
+
+            float output = 0;
+
+            for (int i = 0; i < NumberOfInputs; i++)
+            {
+                string LineOfInput = Console.ReadLine();
+                string[] inputStrings = LineOfInput.Split();
+
+                float QualityOfLife = float.Parse(inputStrings[0]);
+                float NumberOfYears = float.Parse(inputStrings[1]);
+
+                float result = QualityOfLife * NumberOfYears;
+
+                output += result;
+            }
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
+
