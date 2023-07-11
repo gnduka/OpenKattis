441a025e743796692d520a919681acaafc9eca9c
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-11 23:54:59 +0100

    Incomplete task

---
+++ BirthdayMemorisation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Birthday
+{
+    public interface IPerson
+    {
+        string Name();
+        int Preference();
+        int BirthdayDay();
+        int BirthdayMonth();
+    }
+
+    public class Program
+    {
+
+        static void Main()
+        {
+            int NumberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < NumberOfInputs; i++)
+            {
+                string inputString = Console.ReadLine();
+                string[] inputInfo = inputString.Split(' ', '/');
+
+            }
+        }
+
+        class Birthdays
+        {
+
+        }
+    }
+
+    public class BirthdayMemorisation : IPerson
+    {
+        string 
+    }
+}
+
