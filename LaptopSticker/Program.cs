b08cf4812452c8e8091ff82547dc70552c0b5f06
Godwin Nduka <136750604+gnduka@users.noreply.github.com>
2023-07-13 01:27:51 +0100

    LaptopSticker complete

---
+++ LaptopSticker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Laptop
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputString = Console.ReadLine();
+            string[] inputArray = inputString.Split();
+
+            int ComputerWidth = int.Parse(inputArray[0]);
+            int ComputerHeight = int.Parse(inputArray[1]);
+            int StickerWidth = int.Parse(inputArray[2]);
+            int StickerHeight = int.Parse(inputArray[3]);
+
+            if (StickerWidth + 1 < ComputerWidth)
+            {
+                if (StickerHeight + 1 < ComputerHeight)
+                {
+                    Console.WriteLine("1");
+                }
+
+                else
+                {
+                    Console.WriteLine("0");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
